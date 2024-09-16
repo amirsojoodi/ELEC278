a860b8d30110f11c2c32f92f54f81aaad922a18f
amirsojoodi <amir.sojoodi@gmail.com>
2024-09-16 12:25:59 -0400

    Doc: add tutorial 2 problems

---
+++ Tutorials/2024-2025-Fall/Tutorial-02/part1.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+  // Define the size of the array
+  // Feel free to change the size of the 2D array
+  int rows = 3;
+  int cols = 4;
+
+  // To do: Declare and initialize the 2D array to hold int values
+
+  printf("The 2D array is:\n");
+  // To do: Print the array
+
+  return 0;
+}
---
+++ Tutorials/2024-2025-Fall/Tutorial-02/part2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // Feel free to change the size of the 2D array
+  int rows = 3, cols = 3;
+
+  // To do: allocate memory for the array of pointers to rows
+  int **array = ;
+  if (array == NULL) {
+    fprintf(stderr, "Memory allocation failed\n");
+    return 1;
+  } // check if memory allocation failed
+
+  // To do: allocate memory for each row
+  for (int i = 0; i < rows; i++) {
+    // To do
+    if (array[i] == NULL) {
+      fprintf(stderr, "Memory allocation failed\n");
+      return 1;
+    }
+  } // check if memory allocation failed
+
+  // to do: initialize the array with some values
+
+  // To do: print the array
+
+  // To do: free the allocated memory
+
+  return 0;
+}
